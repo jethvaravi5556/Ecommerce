d14bb536a50218fa802e16ce847dda2003830fc2
Ravi Jethva <ravijethva2004@gmail.com>
2026-02-15 12:25:36 +0530

    change in index.jsx file

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -6,20 +6,11 @@ import SignUp from "../pages/SignUp";
 import ForgotPassword from "../pages/ForgotPassword";
 import AdminPanel from "../pages/AdminPanel";
 import AllUsers from "../pages/AllUsers";
-// import AllProducts from "../pages/AllProducts";
-// import CategoryProduct from "../pages/CategoryProduct";
-// import ProductDetails from "../pages/ProductDetails";
-// import SearchProduct from "../pages/SearchProduct";
 import Success from "../pages/Success";
-// import Cart from "../pages/cart";
 import Cancel from "../pages/Cancel";
-// import Order from "../pages/Order";
-// import AllOrders from "../pages/AllOrder";
 import VerifyOtp from "../pages/VarifyOtp";
 import ResetPassword from "../pages/ResetPassword";
-// import ImageSearchResult from "../pages/ImageSearchResult";
-// import SavedItems from "../pages/SavedItems";
-// import SoundDemo from "../components/SoundDemo";
+import Home from "../pages/Home";
 
 const router = createBrowserRouter([
   {
@@ -42,26 +33,7 @@ const router = createBrowserRouter([
         path: "sign-up",
         element: <SignUp />,
       },
-      //   {
-      //     path: "product-category",
-      //     element: <CategoryProduct />,
-      //   },
-      //   {
-      //     path: "product/:id",
-      //     element: <ProductDetails />,
-      //   },
-      //   {
-      //     path: "cart",
-      //     element: <Cart />,
-      //   },
-      //   {
-      //     path: "search",
-      //     element: <SearchProduct />,
-      //   },
-      //   {
-      //     path: "/image-search",
-      //     element: <ImageSearchResult />,
-      //   },
+
       {
         path: "success",
         element: <Success />,
@@ -70,10 +42,7 @@ const router = createBrowserRouter([
         path: "cancel",
         element: <Cancel />,
       },
-      //   {
-      //     path: "order",
-      //     element: <Order />,
-      //   },
+
       {
         path: "/forgot-password",
         element: <ForgotPassword />,
@@ -86,14 +55,7 @@ const router = createBrowserRouter([
         path: "/reset-password",
         element: <ResetPassword />,
       },
-      //   {
-      //     path: "/saved-items",
-      //     element: <SavedItems />,
-      //   },
-      //   {
-      //     path: "/sound-demo",
-      //     element: <SoundDemo />,
-      //   },
+
       {
         path: "admin-panel",
         element: <AdminPanel />,
@@ -102,14 +64,6 @@ const router = createBrowserRouter([
             path: "all-users",
             element: <AllUsers />,
           },
-          //       {
-          //         path: "all-products",
-          //         element: <AllProducts />,
-          //       },
-          //       {
-          //         path: "all-orders",
-          //         element: <AllOrders />,
-          //       },
         ],
       },
     ],

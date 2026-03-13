d31a2522d25ce2c2c9e0de134fa3faca874416bf
Jaimin-vaghela <vparth528@gmail.com>
2026-03-14 00:11:20 +0530

    Resolved merge conflict in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,35 +3,55 @@ import CategoryList from "../components/CategoryList";
 import BannerProduct from "../components/BannerProduct";
 import HorizontalCardProduct from "../components/HorizontalCardProduct";
 import VerticalCardProduct from "../components/VerticalCardProduct";
-<<<<<<< HEAD
 import Footer from "../components/Footer";
-=======
->>>>>>> 8b7df25621484fca19baaff132fe414fc73ebd4f
 
 const Home = () => {
   return (
     <div>
       <CategoryList />
       <BannerProduct />
-      <HorizontalCardProduct category={"airpods"} heading={"Top's Airpodes"} />
+
+      <HorizontalCardProduct
+        category={"airpods"}
+        heading={"Top Airpods"}
+      />
+
       <HorizontalCardProduct
         category={"earphones"}
-        heading={"Popular's Earphones"}
+        heading={"Popular Earphones"}
       />
 
       <VerticalCardProduct
         category={"camera"}
         heading={"Camera & Photography"}
       />
-      <VerticalCardProduct category={"mobile"} heading={"Mobiles"} />
-      <VerticalCardProduct category={"mouse"} heading={"Mouses"} />
-      <VerticalCardProduct category={"printers"} heading={"Printers"} />
-      <VerticalCardProduct category={"speakers"} heading={"Speakers"} />
-      <VerticalCardProduct category={"watches"} heading={"watches"} />
-<<<<<<< HEAD
+
+      <VerticalCardProduct
+        category={"mobile"}
+        heading={"Mobiles"}
+      />
+
+      <VerticalCardProduct
+        category={"mouse"}
+        heading={"Mouses"}
+      />
+
+      <VerticalCardProduct
+        category={"printers"}
+        heading={"Printers"}
+      />
+
+      <VerticalCardProduct
+        category={"speakers"}
+        heading={"Speakers"}
+      />
+
+      <VerticalCardProduct
+        category={"watches"}
+        heading={"Watches"}
+      />
+
       <Footer />
-=======
->>>>>>> 8b7df25621484fca19baaff132fe414fc73ebd4f
     </div>
   );
 };
